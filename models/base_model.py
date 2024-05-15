09c1a7876f98d2219f32bd0eef3a751ecffe8651
HARIMECH Siham <sihamharimech00@gmail.com>
2024-05-15 22:47:53 +0100

    base model 4 and starting 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self, *args, **Kwargs):
         id_base = uuid.uuid4()
@@ -11,15 +12,18 @@ class BaseModel:
         if Kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
     def __str__(self):
         class_name = str(self.__class__.__name__)
         class_dict = str(self.__dict__)
         return f"[{class_name}] ({self.id}) {class_dict}"
+
     def save(self):
         self.updated_at = datetime.now()
 
-s = BaseModel()
-s.name = "My First Model"
-s.number = 89
-s.save()
-print(s)
+    def to_dict(self):
+        dict_vol = self.__dict__
+        dict_vol['__class__'] = self.__class__.__name__
+        dict_vol['created_at'] = self.created_at.isoformat()
+        dict_vol['updated_at'] = self.updated_at.isoformat()
+        return dict_vol
