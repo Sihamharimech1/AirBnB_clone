f90aa1ebc7023bce0129b4e1fab071d6da960a35
yoshimario <sihamharimech00@gmail.com>
2024-05-20 01:54:28 +0100

    console

--- console.py
+++ console.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
+from models import storage
+
 """Console Cmd """
 
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -9,6 +14,8 @@ class HBNBCommand(cmd.Cmd):
     """The AIRBNB command interpreter."""
 
     prompt = '(hbnb) '
+    classes = {"BaseModel": BaseModel,
+               "FileStorage": FileStorage}
 
     def do_EOF(self, line):
         """Exits"""
@@ -22,6 +29,75 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Doesn't execute anything"""
         pass
+
+    def do_create(self, line):
+        """Create instance of a class"""
+        if line == "":
+            print("** class name missing **")
+        else:
+            if line in self.classes:
+                ins = self.classes[line]()
+                ins.save()
+                print(ins.id)
+            else:
+                print("** class doesn't exist **")
+
+    def do_show(self, line):
+        """Show all instances"""
+        param = line.split()
+        taille = len(param)
+        if taille == 0:
+            print("** class name missing **")
+        else:
+            if param[0] not in self.classes:
+                print("** class doesn't exist **")
+            else:
+                if taille < 2:
+                    print("** instance id missing **")
+                else:
+                    un_key = param[0] + '.' + param[1]
+                    objs = storage.all()
+                    if objs.get(un_key) is not None:
+                        print(objs[un_key])
+                    else:
+                        print("** no instance found **")
+
+    def do_destroy(self, line):
+        """Delete Instance"""
+        param = line.split()
+        taille = len(param)
+        if taille == 0:
+            print("** class name missing **")
+        else:
+            if param[0] not in self.classes:
+                print("** class doesn't exist **")
+            else:
+                if taille < 2:
+                    print("** instance id missing **")
+                else:
+                    un_key = param[0] + '.' + param[1]
+                    objs = storage.all()
+                    if objs.get(un_key) is not None:
+                        del objs[un_key]
+                        storage.save()
+                    else:
+                        print("** no instance found **")
+
+    def do_all(self, line):
+        """Show all instances"""
+        param = line.split()
+        taille = len(param)
+        if taille == 0:
+            print("** class name missing **")
+        else:
+            if param[0] not in self.classes:
+                print("** class doesn't exist **")
+            else:
+                first = '["'
+                last = '"]'
+                objs = storage.all()
+                for key in objs:
+                    print(str(first) + str(objs[key]) + str(last))
 
 
 if __name__ == '__main__':
